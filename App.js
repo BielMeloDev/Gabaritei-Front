006ad7825c6cefcc7de128b6eeaaab34635f55a0
Gabriel de Melo Braz <153194983+BielMeloDev@users.noreply.github.com>
2024-10-11 08:53:31 -0300

    simbolo de loading

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, ScrollView, SafeAreaView } from 'react-native';
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator size="larger" />
+      <Image
+        style={styles.img}
+        source={{
+          uri: 'https://t3.ftcdn.net/jpg/04/49/19/08/360_F_449190831_i2whvIQdDIGtuIVWT6QfenWwmRApVJ5l.jpg',
+        }}
+      />
+      <Button
+        onPress={() => Alert.alert('Simple Button pressed')}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <SafeAreaView style={styles.section}>
+        <ScrollView style={styles.scroll}>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    color: 'black',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  img: {
+    width: 300,
+    height: 200,
+  },
+  scroll: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+  section: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  horizontal: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    padding: 20,
   },
 });
