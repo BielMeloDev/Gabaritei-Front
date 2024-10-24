724cf16f9fc893fa931e9d46ce9a7cf59f59dddb
Gabriel de Melo Braz <153194983+BielMeloDev@users.noreply.github.com>
2024-10-24 10:51:24 -0300

    add fundo

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Alert, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, Button, Alert, ScrollView, SafeAreaView } from 'react-native';
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
     <View style={[styles.container]}>
-      <Image
+      {/* <Image
         style={styles.img}
         source={{
           uri: 'https://t3.ftcdn.net/jpg/04/49/19/08/360_F_449190831_i2whvIQdDIGtuIVWT6QfenWwmRApVJ5l.jpg',
         }}
+      /> */}
+      <ImageBackground
+        source={require('./assets/fundo-teste.png')}
+        //source={{ uri: 'https://w7.pngwing.com/pngs/627/467/png-transparent-color-ink-running-people-ink-color-run-thumbnail.png' }}  // Para imagem da internet
+        style={styles.ImageBackground}
       />
+      <SafeAreaView style={styles.section}>
+        <ScrollView style={styles.scroll}>
+          <Text style={styles.text}>
+            Olá, bem-vindo ao
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
       <Button
         onPress={() => Alert.alert('Simple Button pressed')}
         title="Learn More"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
-      <SafeAreaView style={styles.section}>
-        <ScrollView style={styles.scroll}>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </ScrollView>
-      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -40,16 +39,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     color: 'black',
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  img: {
-    width: 300,
-    height: 200,
+  ImageBackground: {
+    flex: 1,
+    alignItems: 'center',
+    resizeMode: 'cover',  // ou 'contain' dependendo de como quer ajustar a imagem
+    justifyContent: 'center',  // centraliza os componentes filhos
+    width: 360,
+    height: 376,
   },
   scroll: {
-    backgroundColor: 'pink',
     marginHorizontal: 20,
   },
   text: {
